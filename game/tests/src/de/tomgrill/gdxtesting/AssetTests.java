1791437d28eaaf3da30f56bac7ce746d2334cc2e
Charles Stubbs <42813301+stubbs774@users.noreply.github.com>
2021-01-29 11:04:58 +0000

    Unit Tests for Assets
    (Bar detailed texture pack check)

--- game/tests/src/de/tomgrill/gdxtesting/AssetTests.java
+++ game/tests/src/de/tomgrill/gdxtesting/AssetTests.java
@@ -115,8 +115,139 @@ public class AssetTests {
     }
 
     //Menu Assets tests
+    @Test
+    public void BackOffExists(){
+        assertTrue("This test will only pass when BackOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/BackOff.png").exists());
+    }
+
+    @Test
+    public void ExitOnExists(){
+        assertTrue("This test will only pass when ExitOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/ExitOn.png").exists());
+    }
+
+    @Test
+    public void ExitOffExists(){
+        assertTrue("This test will only pass when ExitOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/ExitOff.png").exists());
+    }
+
+    @Test
+    public void LevelOffExists(){
+        assertTrue("This test will only pass when LevelOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/LevelOff.png").exists());
+    }
+
+    @Test
+    public void LevelOnExists(){
+        assertTrue("This test will only pass when LevelOn.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/LevelOn.png").exists());
+    }
+
+    @Test
+    public void LoadOffExists(){
+        assertTrue("This test will only pass when LoadOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/LoadOff.png").exists());
+    }
+
+    @Test
+    public void LoadOnExists(){
+        assertTrue("This test will only pass when LoadOn.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/LoadOn.png").exists());
+    }
+
+    @Test
+    public void MenuOffExists(){
+        assertTrue("This test will only pass when MenuOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/MenuOff.png").exists());
+    }
+
+    @Test
+    public void MenuOnExists(){
+        assertTrue("This test will only pass when MenuOn.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/MenuOn.png").exists());
+    }
+
+    @Test
+    public void MissionCompleteExists(){
+        assertTrue("This test will only pass when MissionComplete.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/MissionComplete.png").exists());
+    }
+
+    @Test
+    public void MissionFailedExists(){
+        assertTrue("This test will only pass when MissionFailed.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/MissionFailed.png").exists());
+    }
+
+    @Test
+    public void NewOffExists(){
+        assertTrue("This test will only pass when NewOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/NewOff.png").exists());
+    }
+
+    @Test
+    public void NewOnExists(){
+        assertTrue("This test will only pass when NewOn.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/NewOn.png").exists());
+    }
+
+    @Test
+    public void PauseMenuExists(){
+        assertTrue("This test will only pass when Pause Menu.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/Pause Menu.png").exists());
+    }
+
+    @Test
+    public void PlayOffExists(){
+        assertTrue("This test will only pass when PlayOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/PlayOff.png").exists());
+    }
+
+    @Test
+    public void PlayOnExists(){
+        assertTrue("This test will only pass when PlayOn.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/PlayOn.png").exists());
+    }
+
+    @Test
+    public void SaveOffExists(){
+        assertTrue("This test will only pass when SaveOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/SaveOff.png").exists());
+    }
+
+    @Test
+    public void SaveOnExists(){
+        assertTrue("This test will only pass when SaveOn.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/SaveOn.png").exists());
+    }
+
+    @Test
+    public void TitleExists(){
+        assertTrue("This test will only pass when ExitOff.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Menu/exitOff.png").exists());
+    }
+
     //MiniMap Assets tests
-    //TexturePack Assets tests
+    @Test
+    public void CursorExists(){
+        assertTrue("This test will only pass when Cursor.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Minimap/Cursor.png").exists());
+    }
+
+    @Test
+    public void MapExists(){
+        assertTrue("This test will only pass when Map.png is detected.", Gdx.files
+                .internal("../core/assets/Sprites/Minimap/Map.png").exists());
+    }
+
+    //TexturePack directory Test
+    @Test
+    public void TexturePackExists(){
+        assertTrue("This test will only pass if TexturePack Directory Exists.", Gdx.files.
+                internal("../core/assets/Sprites/TexturePack/").exists());
+    }
 }
 
 
