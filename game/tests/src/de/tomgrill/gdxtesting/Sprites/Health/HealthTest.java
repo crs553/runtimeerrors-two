87454fb0caaea5ad9ad21e1ef245b945ae4ccec5
Charles Stubbs <42813301+stubbs774@users.noreply.github.com>
2021-02-05 21:48:15 +0000

    Added and removed tests

--- game/tests/src/de/tomgrill/gdxtesting/Sprites/Health/HealthTest.java
+++ game/tests/src/de/tomgrill/gdxtesting/Sprites/Health/HealthTest.java
@@ -1,22 +1,31 @@
 package de.tomgrill.gdxtesting.Sprites.Health;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
+import com.team5.game.MainGame;
 import com.team5.game.Sprites.Health.Health;
+import com.team5.game.Sprites.Player;
+import de.tomgrill.gdxtesting.GdxTestRunner;
 import org.junit.runner.RunWith;
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.Spy;
+import org.mockito.*;
 import org.mockito.exceptions.base.MockitoAssertionError;
 import org.mockito.junit.MockitoJUnitRunner;
+import sun.rmi.rmic.Main;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(GdxTestRunner.class)
 public class HealthTest {
+    @Spy
+    MainGame main = mock(MainGame.class);
+    @Spy
+    Player player = mock(Player.class);
 
     @Spy
-    Health health = mock(Health.class,"health");
+    Health health = mock(Health.class, "health");
 
     @Test
     public  void  emptyTest(){
@@ -26,6 +35,7 @@ public class HealthTest {
     @Test
     public void setHealthTest(){
         health.setHealth(3);
-        assertEquals("passes if health has been set",3,health.getHealth());
+        Mockito.when(health.getHealth()).thenReturn(3);
+        assertEquals("Health should be 3 to pass",3, health.getHealth());
     }
 }
