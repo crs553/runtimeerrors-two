bac35c1f0b3c9499588ed76421ffba8d3b579e0c
annabelle565 <72926161+annabelle565@users.noreply.github.com>
2021-02-09 16:55:27 +0000

    docs

--- game/core/src/com/team5/game/Sprites/Pathfinding/Room.java
+++ game/core/src/com/team5/game/Sprites/Pathfinding/Room.java
@@ -9,11 +9,27 @@ public class Room extends Node{
     public String tag;
 
     public Room(String tag, float x, float y, String name) {
+        /*Sets the position and name of the room
+        parameters:
+            String tag gives the tag of the room
+            float x gives the x coordinate of the room
+            float y give the y coordinate of the room
+            String name gives the name of the room
+         */
         super(x, y, name);
         this.tag = tag;
     }
 
     public Room(String tag, float x, float y, String name, float xDim, float yDim) {
+        /*Sets the position and name of the room
+        parameters:
+            String tag gives the tag of the room
+            float x gives the x coordinate of the room
+            float y give the y coordinate of the room
+            String name gives the name of the room
+            float xDim gives x length of the room
+            float yDim gives y length of the room
+         */
         super(x, y, name, xDim, yDim);
         this.tag = tag;
     }
--- game/core/src/com/team5/game/Sprites/Pathfinding/System.java
+++ game/core/src/com/team5/game/Sprites/Pathfinding/System.java
@@ -29,6 +29,15 @@ public class System extends Room{
 
     public System(String tag, float x, float y, String name,
                   float spriteX, float spriteY) {
+        /* Creates the systems, along with a name, tag, and position
+        parameters:
+            String tag gives the tag
+            float x gives x coord of system location
+            float y gives y coord of system location
+            String name gives the name of the system
+            float spriteX gives the x coord of the system sprite location
+            float spriteY gives the y coord of the system sprite location
+         */
         super(tag, x, y, name);
         this.spritePosition = new Vector2(spriteX * Constants.TILE_SIZE,
                 spriteY * Constants.TILE_SIZE);
@@ -36,8 +45,8 @@ public class System extends Room{
         setup();
     }
 
-    //Sets up the system sprites
     public void setup(){
+        /*Sets up the system sprites*/
         broken = false;
 
         onSprite = Constants.ATLAS.findRegion("Systems/" + tag + "On");
@@ -48,6 +57,7 @@ public class System extends Room{
 
     //To be called every frame to draw the system sprites
     public void draw(SpriteBatch batch){
+        /*draws the system sprites in the window*/
         if (broken){
             currentSprite = offSprite;
         }
@@ -55,12 +65,17 @@ public class System extends Room{
         batch.draw(new Texture("Sprites/Minimap/Cursor.png"), x-10, y-10);
     }
 
-    //destroy function is called when the infiltrator breaks the system
+
     public void destroy(){
+        /*destroy function is called when the infiltrator breaks the system*/
         broken = true;
     }
 
     public boolean getBroken(){
+        /*get method for broken
+        returns:
+            boolean broken, which specifies if the system is broken
+         */
         return broken;
     }
 }
