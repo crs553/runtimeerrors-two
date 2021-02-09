19f97c1cdbc42573e534b1870dc65cf8bea3cc79
annabelle565 <72926161+annabelle565@users.noreply.github.com>
2021-02-09 14:21:10 +0000

    docs

--- game/core/src/com/team5/game/Sprites/Health/Health.java
+++ game/core/src/com/team5/game/Sprites/Health/Health.java
@@ -38,6 +38,10 @@ public class Health {
     Vector2 lowerBounds = new Vector2(71*Constants.TILE_SIZE, 82*Constants.TILE_SIZE);
 
     public Health(MainGame game, Player player){
+        /*implements health and healing
+        parameters:
+            MainGame object game, containing the game settings
+            Player object player, implementing the player*/
         this.game = game;
         this.player = player;
 
@@ -51,6 +55,7 @@ public class Health {
 
     //Called every frame, if the player is in the infirmary it's healed every timeToHeal seconds.
     public void update(){
+        /*heals the player and plays the healing animation if the player is in the infirmary*/
         healEffect = anim.getSprite();
         if (player.x >= lowerBounds.x && player.x <= upperBounds.x &&
         player.y >= lowerBounds.y && player.y <= upperBounds.y && currentHealth < maxHealth){
@@ -70,27 +75,46 @@ public class Health {
     }
 
     public int getHealth(){
+        /* getter for the player's health
+        returns:
+            int object giving the player's remaining health
+         */
         return currentHealth;
     }
     // Set Health added by Runtime Errors
     public void setHealth(int newHealth){
+        /*setter for player health
+        parameters:
+            int object newHealth, giving the new value for player health
+         */
         currentHealth = newHealth;
     }
 
     public void increaseHealth(){
+        /* increments the player's health*/
         currentHealth++;
     }
 
     public void decreaseHealth(){
+        /*decrements the player's health and creates the lose screen
+        if the player runs out of health*/
         currentHealth--;
 
         if(currentHealth <= 0) { game.setScreen(new LoseScreen(game)); }
     }
 
-    public boolean getHealing(){return healing;}
+    public boolean getHealing(){
+        /*getter for healing
+        returns:
+            boolean object healing, which is true if the player is in the bounds of
+            the infirmary
+         */
+        return healing;
+    }
 
     //Draws the Healing Effect
     public void draw(SpriteBatch batch, float x, float y){
+        /*draws the healing animation in the window*/
         batch.draw(healEffect, x, y);
     }
 }
