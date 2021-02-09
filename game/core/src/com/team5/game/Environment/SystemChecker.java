5c88aaea78b2f1ec49128a8f2a1f89fab897b87b
annabelle565 <72926161+annabelle565@users.noreply.github.com>
2021-02-09 12:38:18 +0000

    system checker doc

--- game/core/src/com/team5/game/Environment/SystemChecker.java
+++ game/core/src/com/team5/game/Environment/SystemChecker.java
@@ -14,20 +14,29 @@ public class SystemChecker {
     int maxSystems = 15;
 
     public SystemChecker(){
+        /*creates system checker object*/
     }
 
-    //Called when an infiltrator finishes breaking a systems
     public void breakSystem(){
+        /* keeps an integer count of the number of systems broken. Called
+        * when an infiltrator finishes breaking a system*/
         systemsBroken++;
     }
 
-    //Runtime Errors Returns the number of broken systems
+    //Runtime Errors edit
     public int getSystemsBroken(){
+        /*getter for the variable counting the number of systems broken
+        returns:
+            int that counts the num of systems broken
+         */
         return (systemsBroken);
     }
 
-    //Returns true when the systems broken is bigger than or equal to the max number of systems
     public boolean allSystemsBroken(){
+        /*returns true when the system broken is bigger than or equal to the max num of systems
+        returns:
+            boolean value true if all systems are broken, else false.
+         */
         return (systemsBroken>=maxSystems);
     }
 
